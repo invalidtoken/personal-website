7a1284fec44e475237d26e88b10d7392636a9513
invalidtoken <codeit.sahil98@gmail.com>
2020-08-20 00:11:47 +0530

    adding cool styles

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
-import { Basic, Combined, Animated, bounce } from '../shared/styles'
+import { Basic, Combined, Animated, bounce } from "../shared/styles";
 const Home = () => (
   <div>
+    <Basic>Cool Styles</Basic>
     <Basic>Cool Styles</Basic>
     <Combined>
       With <code>:hover</code>.
     </Combined>
     <Animated animation={bounce}>Let's bounce.</Animated>
   </div>
-)
+);
 
-export default Home
+export default Home;
