55f3861c9e13c525b8a2883b4f6c7774da455326
invalidtoken <codeit.sahil98@gmail.com>
2020-08-20 00:24:55 +0530

    adding out in the built bundle

---
+++ out/_next/static/HZULcn9K0RSmJAkHCpnRu/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {"/":["static\u002Fchunks\u002Fpages\u002Findex-403dc25774487546c17f.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-4f0601949c71cd429f69.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
