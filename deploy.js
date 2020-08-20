91788db75dfdd4db2414aa928e2a00ac74bf1dc2
invalidtoken <codeit.sahil98@gmail.com>
2020-08-20 17:36:19 +0530

    changing readme

--- deploy.js
+++ deploy.js
@@ -1,9 +1,17 @@
+/**
+ * gh-pages documentation
+ * Link - https://github.com/tschaub/gh-pages
+ */
 const ghPages = require("gh-pages");
 
 const URL = "https://invalidtoken.github.io/personal-website/";
 
 console.log("Started Publishing");
 
+/**
+ * add option
+ * Link - https://github.com/tschaub/gh-pages#optionsadd
+ */
 ghPages.publish("out", { add: true }, (err) => {
   if (err) {
     console.log("[ERROR] " + err.message);
