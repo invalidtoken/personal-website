5d5a9fb6c371fd08bb614da93268c91e71579f55
invalidtoken <codeit.sahil98@gmail.com>
2020-08-20 16:00:18 +0530

    adding name

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import { Basic, Combined, Animated, bounce } from "../shared/styles";
 const Home = () => (
   <div>
-    <Basic>Cool Styles</Basic>
+    <Basic>Sahil Kapoor Personal Website</Basic>
     <Basic>Cool Styles</Basic>
     <Basic>Cool Styles</Basic>
     <Basic>Cool Styles</Basic>
