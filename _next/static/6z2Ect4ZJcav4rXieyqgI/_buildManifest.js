22c0b4ed58f3dae8eb77038fbc3475234998e457
invalidtoken <codeit.sahil98@gmail.com>
2020-08-20 17:48:18 +0530

    Updates

---
+++ _next/static/6z2Ect4ZJcav4rXieyqgI/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {"/":["static\u002Fchunks\u002Fpages\u002Findex-0ab80ad85c5b877e5548.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-4f0601949c71cd429f69.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
